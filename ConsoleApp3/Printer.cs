7bcda54006f90ceaea81c4a72a42814f5fc0a572
dmitriivin <dimitrii_vinogradov@epam.com>
2022-10-31 20:19:36 +0600

    you are pusya

--- ConsoleApp3/Printer.cs
+++ ConsoleApp3/Printer.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
-    internal class Class1
+    public static class Printer
     {
+        public static void Print()
+        {
+            Console.WriteLine("Ты пуся <3");
+        }
     }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp3;
 
-//Console.WriteLine("Hello, World!");
-//Cicrle cicrle = new Cicrle(4);
-//Console.WriteLine(cicrle.Square);
-object o = 0;
-o.ToString();
-
-Console.WriteLine(Constants.EmilsNotebookModel);
+Printer.Print();
